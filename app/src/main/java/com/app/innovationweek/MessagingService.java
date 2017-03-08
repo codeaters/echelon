a8ba330226e8a0a72d75e8bebc2646666011ffee
Madeyedexter <madeyedexter92@gmail.com>
2017-03-08 20:53:49 +0530

    Added MessagingService for FCM Modified Manifest for newly added MessagingService

---
+++ app/src/main/java/com/app/innovationweek/MessagingService.java
@@ -0,0 +1,44 @@
+package com.app.innovationweek;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+/**
+ * Created by Madeyedexter on 08-03-2017.
+ */
+
+public class MessagingService extends FirebaseMessagingService {
+
+    private static final String TAG= "MessagingService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // ...
+
+
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        Map<String, String> data = remoteMessage.getData();
+        if (data.containsKey("click_action")) {
+            NotificationActionHelper.startActivity(data.get("click_action"), null, this);
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+}
