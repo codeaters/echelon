fb467a8dd8a8664135bcb3bf0434992e70f034e7
madeyedexter <madeyedexter92@gmail.com>
2017-03-10 22:52:26 +0530

    Refacto : Implemented parcelable on Event model Modified EventFragment to handle Event model as Parcelable Added News model

--- app/src/main/java/com/app/innovationweek/model/Event.java
+++ app/src/main/java/com/app/innovationweek/model/Event.java
@@ -1,72 +1,202 @@
 package com.app.innovationweek.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Id;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.annotation.ToMany;
+import org.greenrobot.greendao.DaoException;
+import com.app.innovationweek.model.dao.DaoSession;
+import com.app.innovationweek.model.dao.RuleDao;
+import com.app.innovationweek.model.dao.EventDao;
 
 
 /**
  * Created by zeeshan on 3/8/2017.
  */
 @Entity
-public class Event {
+public class Event implements Parcelable{
     @Id
     private String id;
     private String imageUrl;
     private String name;
     private String description;
     private long startDate;
-    private String rule;
-    @Generated(hash = 831176215)
+
+    @ToMany(referencedJoinProperty = "eventId")
+    private List<Rule> rules;
+    /** Used to resolve relations */
+    @Generated(hash = 2040040024)
+    private transient DaoSession daoSession;
+    /** Used for active entity operations. */
+    @Generated(hash = 1542254534)
+    private transient EventDao myDao;
+
+    @Generated(hash = 1670786169)
     public Event(String id, String imageUrl, String name, String description,
-            long startDate, String rule) {
+            long startDate) {
         this.id = id;
         this.imageUrl = imageUrl;
         this.name = name;
         this.description = description;
         this.startDate = startDate;
-        this.rule = rule;
     }
+
     @Generated(hash = 344677835)
     public Event() {
     }
+
+    protected Event(Parcel in) {
+        id = in.readString();
+        imageUrl = in.readString();
+        name = in.readString();
+        description = in.readString();
+        startDate = in.readLong();
+        rules = in.createTypedArrayList(Rule.CREATOR);
+    }
+
+    public static final Creator<Event> CREATOR = new Creator<Event>() {
+        @Override
+        public Event createFromParcel(Parcel in) {
+            return new Event(in);
+        }
+
+        @Override
+        public Event[] newArray(int size) {
+            return new Event[size];
+        }
+    };
+
     public String getId() {
         return this.id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public String getImageUrl() {
         return this.imageUrl;
     }
+
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
+
     public String getName() {
         return this.name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDescription() {
         return this.description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public long getStartDate() {
         return this.startDate;
     }
+
     public void setStartDate(long startDate) {
         this.startDate = startDate;
     }
-    public String getRule() {
-        return this.rule;
-    }
-    public void setRule(String rule) {
-        this.rule = rule;
+
+    /**
+     * To-many relationship, resolved on first access (and after reset).
+     * Changes to to-many relations are not persisted, make changes to the target entity.
+     */
+    @Generated(hash = 1735897230)
+    public List<Rule> getRules() {
+        if (rules == null) {
+            final DaoSession daoSession = this.daoSession;
+            if (daoSession == null) {
+                throw new DaoException("Entity is detached from DAO context");
+            }
+            RuleDao targetDao = daoSession.getRuleDao();
+            List<Rule> rulesNew = targetDao._queryEvent_Rules(id);
+            synchronized (this) {
+                if (rules == null) {
+                    rules = rulesNew;
+                }
+            }
+        }
+        return rules;
     }
 
+    /** Resets a to-many relationship, making the next get call to query for a fresh result. */
+    @Generated(hash = 53842526)
+    public synchronized void resetRules() {
+        rules = null;
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#delete(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 128553479)
+    public void delete() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.delete(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#refresh(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 1942392019)
+    public void refresh() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.refresh(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#update(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 713229351)
+    public void update() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.update(this);
+    }
+
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 1459865304)
+    public void __setDaoSession(DaoSession daoSession) {
+        this.daoSession = daoSession;
+        myDao = daoSession != null ? daoSession.getEventDao() : null;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(imageUrl);
+        dest.writeString(name);
+        dest.writeString(description);
+        dest.writeLong(startDate);
+        dest.writeTypedList(rules);
+    }
 }
