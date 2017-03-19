3d2478b863277b97c7d1b7bf79c444771a7af12f
Zeeshan Khan <zkhan19093@gmail.com>
2017-03-20 01:42:12 +0530

    refacot: adding imports missed while merging

--- app/src/main/java/com/app/innovationweek/LeaderboardActivity.java
+++ app/src/main/java/com/app/innovationweek/LeaderboardActivity.java
@@ -13,8 +13,10 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.app.innovationweek.adapter.LeaderboardAdapter;
 import com.app.innovationweek.callbacks.DaoOperationComplete;
